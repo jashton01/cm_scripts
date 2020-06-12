eb71bbdeae87aca6e1c827cddee378993cbba101
James Ashton <ashtonj@gmail.com>
2020-06-11 21:40:43 -0400

    Update first_cm_script.py
    removing supplementary code to focus on client add workflow

--- first_cm_script.py
+++ first_cm_script.py
@@ -2,17 +2,6 @@ from createsend import *
 
 auth = {'api_key':'otYR1hX2Rwul5NfryE/34LF2dpnEq54yCYn3ezdGVsEx5m6Ii3JJ0xp1+RpBPsnD2M3Nw7O2fkWTfZTFhGsK6YfZ2bFe2q/f0BVMLC1M08x45xlTEi3KSzjzASFxtxp0Wlepoi9SVYr6ga/vF7bd4A=='}
 
-cs=CreateSend(auth)
-clients = cs.clients()
-#subscribers = cs.subscribers()
-
-
-print(clients)
-print(clients.client_id)
-
-for cl in clients:
-  print("Client: %s" % cl.Name)
-  #print("Client ID: %s" % cl.client_id)
 
 my_list = List(auth)
 
