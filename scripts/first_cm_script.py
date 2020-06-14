a592992ff7424d81706be1e865d9d7357a621120
James Ashton <ashtonj@gmail.com>
2020-06-14 07:54:33 -0400

    catchup commit
    reorganized folder structure. JS testing.

---
+++ scripts/first_cm_script.py
@@ -0,0 +1,21 @@
+from createsend import *
+
+
+account_auth = {'api_key':'/7k+rgSybkxGMa64aUb+DPuAGpM5NxC3EjanNlSDjrRom3gKTHe6Z/t5GOJA9IlditAvifnjymZGVH6ZW1xqYE5EnuoJBr9hWXn7yscIZOyoeoJjugUcGix/fb8V2lzJIG+ab56sLijjsgL49KGvWw=='}
+client_auth = {'api_key':'otYR1hX2Rwul5NfryE/34LF2dpnEq54yCYn3ezdGVsEx5m6Ii3JJ0xp1+RpBPsnD2M3Nw7O2fkWTfZTFhGsK6YfZ2bFe2q/f0BVMLC1M08x45xlTEi3KSzjzASFxtxp0Wlepoi9SVYr6ga/vF7bd4A=='}
+
+
+#my_list = List(client_auth)
+
+#my_list.create("2bd21ec1091ba743b483563a25062a61", "API List 4", "", False, "")
+
+
+#my_subscriber = Subscriber(client_auth)
+
+#custom_fields = [{"Key": 'website', "Value": 'http://example.com/'}]
+
+#my_subscriber.add("7c480b336839fbba731ca50c5d269666", "jashton+api3@sailthru.com", "Subscriber", custom_fields, True, "yes")
+
+my_client = Client(account_auth)
+
+my_client.create("Z API Test Client", "(GMT+10:00) Canberra, Melbourne, Sydney", "Australia")
---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+
+
+/*
+Test Event Handling
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+*/
+
+let myImage = document.querySelector('img');
+console.log(myImage);
+let myNavBar = document.querySelector('nav');
+console.log(myNavBar);
+
+/*
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cm.png') {
+      myImage.setAttribute ('src','images/sailthru.png');
+    } else {
+      myImage.setAttribute ('src','images/cm.png');
+    }
+}
+*/
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('p');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to CM, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+/*
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/cm.png') {
+      myImage.setAttribute ('src','images/sailthru.png');
+    } else {
+      myImage.setAttribute ('src','images/cm.png');
+    }
+}
+*/
